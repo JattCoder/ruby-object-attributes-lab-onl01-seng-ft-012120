c25ba6eaf2149c2cc4e0339262d10a5ea09bd4dc
Harmandeep Mand <harmandeepmand.hm@gmail.com>
2020-01-28 04:42:42 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -5,9 +5,19 @@ class Person
   def name
     @name
   end
+  def job=(personjob)
+    @job = personjob
+  end
+  def job
+    @job
+  end
 end
 beyonce = Person.new
 beyonce.name = "Beyonce"
 beyonce.instance_variable_get(:@name)
 beyonce.instance_variable_set(:@name,"Beyonce")
 beyonce.name
+beyonce.job = "Singer"
+beyonce.instance_variable_get(:@job)
+beyonce.instance_variable_set(:@job,"Singer")
+beyonce.job
