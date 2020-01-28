61e989cdac97e6db39063d3522c4d6f48d0193f4
Harmandeep Mand <harmandeepmand.hm@gmail.com>
2020-01-28 04:33:16 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,15 +1,8 @@
 class Dog
-  def initialize(name)
-    @name = name
-  end
   def name=(dogname)
     @name = dogname
-  end
-  def name
-    @name
   end
 end
 fido = Dog.new
 fido.name = "Fido"
 fido.instance_variable_get(:@name)
-fido.name
